937f9bff8e04d9f2b6acf1b6bf672b1c7f8edd58
steller520 <shivamnegi571@gmail.com>
2025-11-13 10:37:03 +0530

    Add initial project configuration and implement basic Selenium setup
    - Create settings.json for Java build configuration
    - Update pom.xml with necessary dependencies for Selenium and TestNG
    - Implement basic Selenium WebDriver usage in App.java
    - Add sample TestNG test class
    - Remove unused JUnit test class

--- selenium.automation.framework/src/main/java/selenium/automation/framework/App.java
+++ selenium.automation.framework/src/main/java/selenium/automation/framework/App.java
@@ -1,5 +1,8 @@
 package selenium.automation.framework;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
     }
 }
---
+++ selenium.automation.framework/src/test/java/selenium/automation/framework/test.java
@@ -0,0 +1,12 @@
+package selenium.automation.framework;
+
+import org.testng.annotations.Test;
+
+public class test {
+    @Test
+    public void sampleTest() {
+        System.out.println("This is a sample test method.");
+    }
+}
+
+    
